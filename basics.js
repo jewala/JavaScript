00d000e619542e216710d988089fd9e959dbc31c
jewala <89640417+jewala@users.noreply.github.com>
2023-02-20 09:52:55 +0000

    added first function myAge

--- basics.js
+++ basics.js
@@ -5,15 +5,14 @@ let lastName = 'Wagner'
 console.log('His last name is ' + lastName + '.')
 
 
-let age = 15
+let age = 30
 console.log(firstName + ' is ' + age + ' years old.')
 
-let isMinor
-
-if (age < 18){
-    isMinor = true
-}
-else{
-    isMinor = false
-}
+let isMinor = (age > 18) ? true : false
 console.log('User is a minor: ' + isMinor)
+
+function myAge (a, b){
+    return a * b
+}
+let result = myAge(5, 6)
+console.log(result)
